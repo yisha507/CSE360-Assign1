997f0fae8a3a8cf7518dada04841615c70b5d140
yisha <yisha@YEE>
2020-10-02 19:43:06 -0700

    Added method bodies and "history" variable of type String

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -6,35 +6,42 @@ package cse360assignment02;
  */
 public class AddingMachine {
 	private int total;
+	private String history;
+	
 	  /**
-	   * Constructs a adding machine
+	   * Constructs a adding machine with initial total and history
 	   */
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    history = "0";
 	  }
 	  /**
 	   * Returns the total value as an int
 	   */
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  /**
-	   * Adds a number to int value
+	   * Adds a number to int value and updates history
 	   * @param value the value of the current total
 	   */
 	  public void add (int value) {
+		  total += value;
+		  history = history + " + " + (value);
 	  }
 	  /**
-	   * Subtracts a number from int value
+	   * Subtracts a number from int value and updates history
 	   * @param value the value of the current total
 	   */
 	  public void subtract (int value) {
+		  total -= value;
+		  history = history + " - " + (value);
 	  }
 	  /**
 	   * Returns history of additions & subtractions applied to int value
 	   */
 	  public String toString () {
-	    return "";
+	    return history;
 	  }
 	  /**
 	   * Unused fuction 
