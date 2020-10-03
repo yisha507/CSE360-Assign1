29eb8e45e90e8c7ceac39a2a4ca5ede1e50fe060
yisha <yisha@YEE>
2020-10-02 18:28:51 -0700

    CSE 360 Assign 1

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,44 @@
+package cse360assignment02;
+/**
+ * Program to add and subtract to a total value, and return it as string or int
+ * @author yisha
+ *
+ */
+public class AddingMachine {
+	private int total;
+	  /**
+	   * Constructs a adding machine
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  /**
+	   * Returns the total value as an int
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  /**
+	   * Adds a number to int value
+	   * @param value the value of the current total
+	   */
+	  public void add (int value) {
+	  }
+	  /**
+	   * Subtracts a number from int value
+	   * @param value the value of the current total
+	   */
+	  public void subtract (int value) {
+	  }
+	  /**
+	   * Returns history of additions & subtractions applied to int value
+	   */
+	  public String toString () {
+	    return "";
+	  }
+	  /**
+	   * Unused fuction 
+	   */
+	  public void clear() {
+	  }
+	}
